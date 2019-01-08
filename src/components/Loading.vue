42bb129afd79667d0092b557fbe386fd01a4a9f1
RBlanchet <contact@blanchet-romain.fr>
2019-01-08 15:03:14 +0100

    First commit

---
+++ src/components/Loading.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="loading">
+    <!--<img src="~/assets/img/logo.svg" alt="Chargement" class="loading__image"/>-->
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Loading"
+    }
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Loading/>
+  </div>
+</template>
+
+<script>
+  import Loading from "@/components/Loading.vue"
+
+  export default {
+    name: "Index",
+    components: {
+      Loading
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../style/main";
+</style>
